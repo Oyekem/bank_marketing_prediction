d934dd6d3646eb6ce4d9e324b79f6f90514b779d
Tawakalt Oyeleso <129989054+Oyekem@users.noreply.github.com>
2026-04-10 14:08:44 +0100

    Update preprocessing.py

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -1,1 +1,19 @@
+# preprocessing.py
 
+
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+
+def create_preprocessor(categorical_features, numerical_features):
+    categorical_transformer = OneHotEncoder(handle_unknown='ignore')
+    numerical_transformer = StandardScaler()
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            ("cat", categorical_transformer, categorical_features),
+            ("num", numerical_transformer, numerical_features)
+        ]
+    )
+
+    return preprocessor
