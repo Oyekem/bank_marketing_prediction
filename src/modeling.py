561ad74e2f8e1ede84b54961a5d249081508404a
Tawakalt Oyeleso <129989054+Oyekem@users.noreply.github.com>
2026-04-10 14:09:51 +0100

    Update modeling.py

--- src/modeling.py
+++ src/modeling.py
@@ -1,1 +1,78 @@
+#modeling.py
 
+import xgboost as xgb
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from imblearn.over_sampling import SMOTE
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    classification_report,
+    roc_auc_score,
+    roc_curve,
+    auc
+)
+
+# Handle Imbalance(SMOTE)
+
+def balance_data(X_train, y_train):
+    smote = SMOTE(random_state=42)
+    X_res, y_res = smote.fit_resample(X_train, y_train)
+
+    print("Class distribution after SMOTE:")
+    print(pd.Series(y_res).value_counts())
+
+    return X_res, y_res
+
+# Train Model
+def train_model(X_train, y_train):
+    model = xgb.XGBClassifier(
+        n_estimators=200,
+        max_depth=5,
+        learning_rate=0.1,
+        eval_metric="logloss",
+        random_state=42
+    )
+
+    model.fit(X_train, y_train)
+    return model
+
+
+# Prediction
+def get_predictions(model, X_test):
+    y_pred = model.predict(X_test)
+    y_prob = model.predict_proba(X_test)[:, 1]
+    return y_pred, y_prob
+
+
+# Evaluation
+def evaluate_model(y_test, y_pred, y_prob):
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+
+    print("\nConfusion Matrix:\n", confusion_matrix(y_test, y_pred))
+
+    print("\nClassification Report:\n", classification_report(y_test, y_pred))
+
+    roc_auc = roc_auc_score(y_test, y_prob)
+    print("\nROC-AUC Score:", roc_auc)
+
+    return roc_auc
+
+
+
+# ROC CURVE PLOT
+
+def plot_roc_curve(y_test, y_prob):
+    fpr, tpr, _ = roc_curve(y_test, y_prob)
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure(figsize=(6, 5))
+    plt.plot(fpr, tpr, label=f"AUC = {roc_auc:.3f}")
+    plt.plot([0, 1], [0, 1], linestyle="--")
+
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.title("XGBoost ROC Curve")
+    plt.legend()
+    plt.show()
